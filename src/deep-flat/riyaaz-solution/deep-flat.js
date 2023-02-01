52c3b78d5ba3049ab550a44107d4c3ccfa1bbe34
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-01 14:05:34 +0200

    add js doc to code for deep flat

--- src/deep-flat/riyaaz-solution/deep-flat.js
+++ src/deep-flat/riyaaz-solution/deep-flat.js
@@ -1,1 +1,19 @@
+/** .........
+ * Converts a nested array into a single array with no nesting.
+ *
+ * It returns a new array and there are no side-effects.
+ *
+ * @param {Array} [array=[]] - The array to flatten.
+ * @returns {Array} Returns the new flattened array.
+ * @example
+ *
+ * deepFlat([1, [2, [3, [4]], 5]]);
+ * // -> [1, 2, 3, 4, 5]
+ *
+ * @example
+ *
+ * deepFlat(['a', ['b', [['c'], ['d']], 'e']]);
+ * // -> ['a', 'b', 'c', 'd', 'e']
+ */
+
 export const flattenArray = (arr) => arr.flat(10);
