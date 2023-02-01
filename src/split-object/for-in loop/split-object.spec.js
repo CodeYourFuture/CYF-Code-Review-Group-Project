19b34d830c6403f392df6324a45b212d12b12a29
AlexJora <jora.catalinaa@gmail.com>
2023-02-01 12:30:55 +0200

    changed test file

--- src/split-object/for-in loop/split-object.spec.js
+++ src/split-object/for-in loop/split-object.spec.js
@@ -1,22 +1,34 @@
-const testObject = {
-  name: 'John',
-  age: 30,
-  city: 'London',
-};
-//test object
-const expectedOutput = [{ name: 'John' }, { age: 30 }, { city: 'London' }];
-//we call the splitObjectIntoArray() function with the test object, and we save the result to a variable.
-const result = splitObjectIntoArray(testObject);
-//test checks if the length of the result array is equal to the length of the expected output array.
-console.assert(
-  result.length === expectedOutput.length,
-  'Array length is incorrect',
-);
-//test loops through both arrays, and for each iteration, checks if the current key-value pair in both arrays is equal.
-for (let i = 0; i < result.length; i++) {
-  console.assert(
-    result[i][Object.keys(result[i])[0]] ===
-    expectedOutput[i][Object.keys(expectedOutput[i])[0]],
-    'Key value pair is incorrect',
-  );
-}
+import { objectToArray } from './split-object.js';
+
+describe('Object to Array', () => {
+  it('Converts an object to an array', () => {
+    const input = { name: 'John', age: 30 };
+    const expectedOutput = [{ name: 'John' }, { age: 30 }];
+    expect(objectToArray(input)).toEqual(expectedOutput);
+  });
+
+  it('Handles an empty object', () => {
+    const input = {};
+    const expectedOutput = [];
+    expect(objectToArray(input)).toEqual(expectedOutput);
+  });
+
+  it('Converts a complex object to an array', () => {
+    const input = { name: 'John', age: 30, hobbies: ['reading', 'traveling'] };
+    const expectedOutput = [{ name: 'John' }, { age: 30 }, { hobbies: ['reading', 'traveling'] },
+    ];
+    expect(objectToArray(input)).toEqual(expectedOutput);
+  });
+
+  it('Handles a numeric object', () => {
+    const input = { 1: 'one', 2: 'two', 3: 'three' };
+    const expectedOutput = [{ 1: 'one' }, { 2: 'two' }, { 3: 'three' }];
+    expect(objectToArray(input)).toEqual(expectedOutput);
+  });
+
+  it('Handles an object with nested objects', () => {
+    const input = { name: 'John', details: { age: 30, city: 'New York' } };
+    const expectedOutput = [{ name: 'John' }, { details: { age: 30, city: 'New York' } },];
+    expect(objectToArray(input)).toEqual(expectedOutput);
+  });
+});
